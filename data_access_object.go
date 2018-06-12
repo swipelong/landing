9fa4f828ae5f296ca5addee15593e55b0351ae04
Alexander Kleinhans <alexanderkleinhans@protonmail.com>
2018-06-12 15:58:20 -0600

    Adding email template, sendgrid api, and encryptied environment variables file.

--- data_access_object.go
+++ data_access_object.go
@@ -3,8 +3,12 @@ package main
 import (
 	"database/sql"
 	_ "github.com/lib/pq"
+	"os"
 )
 
 func DAO() (*sql.DB, error) {
-	return sql.Open("postgres", "user=postgres dbname=swipelong sslmode=disable")
+	var user = os.Getenv("PG_USER")
+	var pass = os.Getenv("PG_PASS")
+	var conn_string = "user=" + user + " password=" + pass + " dbname=swipelong sslmode=disable"
+	return sql.Open("postgres", conn_string)
 }
--- emailer.go
+++ emailer.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"errors"
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
 	"html/template"
 	"os"
 )
@@ -13,6 +16,7 @@ var email_templates = template.Must(template.ParseFiles(
 
 type WelcomeEmail struct {
 	EmailAddress string
+	Content      string
 }
 
 // Emailer
@@ -21,13 +25,36 @@ type Emailer struct{}
 
 func (e Emailer) Send(email_address string, template string) error {
 	if template == "welcome" {
-		we := new(WelcomeEmail)
-		we.EmailAddress = email_address
-		err := email_templates.ExecuteTemplate(os.Stdout, "welcome.html", we)
+		err := e.sendWelcomeEmail(email_address)
 		if err != nil {
 			return err
 		}
 		return nil
 	}
 	return errors.New("Unrecognized email template.")
+}
+
+func (e Emailer) sendWelcomeEmail(email_address string) error {
+	// Create the welcome email.
+	we := new(WelcomeEmail)
+	we.EmailAddress = email_address
+	var template_buffer bytes.Buffer
+	err := email_templates.ExecuteTemplate(&template_buffer, "welcome.html", we)
+	if err != nil {
+		return err
+	}
+	we.Content = template_buffer.String()
+	// Use the SendGrid API to send the email.
+	from := mail.NewEmail("SwipeLong", "welcome@swipelong.com") // @TODO put in app config.
+	subject := "Welcome to SwipeLong!"
+	to := mail.NewEmail("", we.EmailAddress)
+	plainTextContent := "Welcome to SwipeLong!" // backup
+	htmlContent := we.Content
+	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
+	_, err = client.Send(message) // _ = response. ask client, maybe later we'll log the response.
+	if err != nil {
+		return err
+	}
+	return nil
 }
